e4c9f84b8b917fb2995941765d27b2600f837970
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-07-13 19:25:37 -0400

    feat: mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const ENDPOINT = new URL('http://localhost:8080/')
+
+const CHAT_ENDPOINT = new URL(`${ENDPOINT.href}scotty/v1/chat`)
 
 function App() {
+  const [post, setPost] = useState(<li></li>);
+  const [userPrompt, setUserPrompt] = useState('');
+
+  useEffect(() => {
+    if (!userPrompt) return
+    axios.post(CHAT_ENDPOINT.href, { message: userPrompt }, { headers: { "Content-Type": 'application/json' } }).then(res => {
+      setPost((res.data?.messages ?? []).map((value: { message: string }) => <li>{value.message}</li>))
+    })
+  }, [userPrompt])
+
+  console.log(post);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <Formik
+        initialValues={{ prompt: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setUserPrompt(values.prompt)
+
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="input" name="prompt" />
+
+            <ErrorMessage name="prompt" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+      <ul>{post}</ul>
+
     </div>
   );
 }
